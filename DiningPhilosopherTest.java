111728514cc1731bcb9afe0f2c7ffef5f3d55fa1
tjmoore06 <45438970+tjmoore06@users.noreply.github.com>
2019-04-22 22:29:48 -0500

    Update DiningPhilosopherTest.java
    few edits and added comments

--- DiningPhilosopherTest.java
+++ DiningPhilosopherTest.java
@@ -1,15 +1,27 @@
+/*
+  Program: DiningPhilosopherTest.java
+  Authors: James Hund, TJ Moore
+  Version: 04/22/19
+  Description: This program is an implementation of Algorithm 6.12 for the
+               "Dining Philosophers Problem". It takes as arguments the number
+               of rounds and number of philosophers in that order.
+ */
 public class DiningPhilosopherTest {
   public static void main(String[] args) {
+    // initializes number of rounds and philosophers
     int rounds = Integer.parseInt(args[0]);
     int n = Integer.parseInt(args[1]);
+    // Creating and initializing forks
     Fork[] fork = new Fork[n];
     for(int i = 0; i < fork.length; i++){
       fork[i] = new Fork("F: " + i);	 	 
     }
+    // Creating and initializing Philosophers
     Philosopher[] philosophers = new Philosopher[n];
     for(int i=0;i<n-1;i++){
       philosophers[i] = new Philosopher("P: "+i+" -", fork[i], fork[i+1], rounds);
     }
+    // nth Philosopher grabs from the opposite side first
     philosophers[n-1] = new Philosopher("P: 4 - ",fork[0],fork[n-1],rounds);
     for(int i=0;i<philosophers.length;i++){
       log.record("Philosopher "+i+" has entered the battle");
@@ -19,22 +31,23 @@ public class DiningPhilosopherTest {
   }
 }
 
+// fork class
 class Fork{
   public boolean used;
   public String name;
 
-  public Fork(){
-  }
   public Fork(String fname){
     this.name = fname;
   }
 
   public synchronized void take() {
+    // for output
     log.record("Fork "+name+" is being used");
     this.used = true;
   }
 
   public synchronized void release() {
+    // for output
     log.record("Fork "+name+" is being released");
     this.used = false;
   }
@@ -43,26 +56,43 @@ class Fork{
 class Philosopher extends Thread {
   private Fork First;
   private Fork Second;
+  private boolean hasFork = false;
   private String name;
-  private int state;
   private int round;
 
+  // the init of Philosopher
   public Philosopher(String name, Fork left, Fork right, int rounds) {
-    this.state = 1;
     this.name = name;
     Second = left;
     First = right;
     round = rounds; 
   }
 
+  // starts the Philosopher
   public void run(){
     for(int i=0; i<=round; i++){
-      eat();
+      think();
+
+      // looks for first fork
+      if(! First.used) {
+        First.take();
+        hasFork=true;
+      }
+      // looks to see if second fork is free and if first fork is already taken by this instance
+      if(! Second.used && hasFork) {
+        Second.take();
+        eat();
+        // releasing the forks
+        First.release();
+        Second.release();
+        hasFork=false;
+      }
     }
   }
 
-  public void think() {
-    this.state = 1;
+  // think method
+  public void think(){
+    // for output
     log.record(name + " think");
     try {
       sleep((int)(Math.random() * 100));
@@ -71,23 +101,18 @@ class Philosopher extends Thread {
     }
   }
 
+  // eat method
   public void eat(){
-    if(! Second.used){
-      Second.take();
-      First.take();
-      log.record(name + " eat");
-      try {
-        sleep((int)(Math.random() * 100));
-      }
-      catch(InterruptedException e){
-      }
-      First.release();
-      Second.release();
+    // for output
+    log.record(name + " eat");
+    try {
+      sleep((int)(Math.random() * 100));
     }
-    think();
+    catch(InterruptedException e){
+    }
   }
 }
-	
+// prints output to command line
 class log{
   public static void record(String msg){
     System.out.println(msg);
